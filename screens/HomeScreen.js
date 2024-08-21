4a179fd2cbbf78993b87b08c67f5c18450fe003c
Jess Puri <jesspuri121@hotmail.com>
2024-08-21 11:30:35 +0530

    Create HomeScreen.js

---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to TEMU App!</Text>
+      <Button
+        title="Go to Product"
+        onPress={() => navigation.navigate('Product')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
